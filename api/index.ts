66695cbdec713eabd6035392311171170949abdc
Bussa Kedaar <kedaarbussa2604@gmail.com>
2026-09-07 15:53:59 +0530

    Export module.exports = app for CJS Vercel serverless function invocation

--- api/index.ts
+++ api/index.ts
@@ -105,4 +105,5 @@ app.use((err: any, req: any, res: any, next: any) => {
   res.status(500).json({ message: err?.message || 'Internal Server Error' });
 });
 
+module.exports = app;
 export default app;
